d2fbe29cad6746bbc5f720b88aa831f183b9770a
andre.kortkamp <andre.kortkamp@genezys.io>
2023-07-25 14:59:17 -0300

    add transactions

---
+++ contracts/MultiSig.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract MultiSig {
+    address[] public owners;
+    uint256 public required;
+    uint256 public transactionCount;
+
+    struct Transaction {
+     address destination;
+     uint256 value;
+     bool executed;   
+    }
+
+    mapping(uint256 => Transaction) public transactions;
+
+    constructor(address[] memory _owners, uint256 _required) {
+        require(_owners.length > 0);
+        require(_required > 0);
+        require(_required <= _owners.length);
+        owners = _owners;
+        required = _required;
+    }
+
+    function addTransaction(address _destination, uint256 _value) public returns (uint256 transactionId) {
+        transactionId = transactionCount;
+        transactions[transactionCount] = Transaction(_destination, _value, false);
+        transactionCount += 1;
+    }
+}
